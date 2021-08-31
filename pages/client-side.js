3ed53b94a33416856fe444b787e83eae534f4060
Shubham Prakash <28767301+iShubhamPrakash@users.noreply.github.com>
2021-08-31 23:58:14 +0530

    Fix build error

--- pages/client-side.js
+++ pages/client-side.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Main.module.css";
 import ClientOnly from "../components/ClientOnly";
 import Countries from "../components/Countries";
 
